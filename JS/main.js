11f4aed5f66d5a9dd57894b52ee7a26d3c1b937d
Vladislav899913 <84039641+Vladislav899913@users.noreply.github.com>
2022-11-23 00:19:30 +0300

    3_day_GIT

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,3 @@
-// let header = document.querySelector(".js-header"),
-//   headerH = document.querySelector(".js-header").clientHeight;
-
-// document.onscroll = function () {
-//   let scroll = window.scrollY;
-//   if (scroll > headerH) {
-//     header.classList.add("fixed");
-//     document.body.style.paddingTop = headerH + "px";
-//   } else {
-//     header.classList.remove("fixed");
-//     document.body.removeAttribute("style");
-//   }
-// };
-
 const hamb = document.querySelector("#hamb");
 const popup = document.querySelector("#popup");
 const nav_links = document.querySelector("#nav_links").cloneNode(1);
@@ -44,15 +30,98 @@ for (let anchor of anchors) {
   });
 }
 
-let swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active_dot", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active_dot";
+}
+
+// function formAlert() {
+//   let answer = confirm("Вы уверены в корректности введённых данных?");
+//   if (answer == true) {
+
+//   }
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+    let error = formValidate(form);
+    if (error === 0) {
+      // form.classList.add("_sending");
+      let response = await fetch("sendmail.php", {
+        method: "POST",
+        body: formData,
+      });
+      if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+        formPreview.innerHTML = "";
+        form.reset;
+        // form.classList.remove("_sending");
+      } else {
+        alert("Ошибка!");
+        // form.classList.remove("_sending");
+      }
+    } else {
+      alert("Заполните обязательные поля!");
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+      if (input.classList.contains("_email")) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
 });
